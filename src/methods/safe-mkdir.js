5a8b1a60387af9aeffc2c26b738bdf3b7a561479
Sett Sarverott A.A.B <sarverott@gmail.com>
2026-03-17 05:43:21 +0100

    adding husky listeners, biome stuff, jest tests, testing first of methods - bringing repo to pro lvl

--- src/methods/safe-mkdir.js
+++ src/methods/safe-mkdir.js
@@ -2,27 +2,39 @@ const fs = require("fs");
 const path = require("path");
 
 function safeMkdir(...pathchain){ /* from main.js::LINES[173:196] then rewritten */
-    try{
-        fs.accessSync(
-            path.resolve(
-                ...pathchain
+    const resolvedPath = path.resolve(...pathchain)
             // __dirname, 
             //     "../dataset",
             //     "./config"
-            ),
+            //)
+    try{
+        fs.accessSync(
+            resolvedPath,
+            // path.resolve(
+            //     ...pathchain
+            // // __dirname, 
+            // //     "../dataset",
+            // //     "./config"
+            // ),
             fs.constants.R_OK|fs.constants.W_OK
         );
+        return [false, resolvedPath, []];
     }catch(err){
-        console.debug(err)
-
-        fs.mkdirSync(
-            path.resolve(
-                ...pathchain
-                    // __dirname, 
-                    // "../dataset",
-                    // "./config"
-            )
-        );
+        //console.debug(err)
+        try{
+            fs.mkdirSync(
+                resolvedPath
+                // path.resolve(
+                //     ...pathchain
+                //         // __dirname, 
+                //         // "../dataset",
+                //         // "./config"
+                // )
+            );
+            return [true, resolvedPath, [err]];
+        }catch(unpredictedErr){
+            return [false, resolvedPath, [err, unpredictedErr]];
+        }
     }
 }
 
---
+++ tests/safe-mkdir.test.js
@@ -0,0 +1,34 @@
+const safeMkdir = require("../src/methods/safe-mkdir.js");
+const fs = require("fs")
+const path = require("path")
+const UNIXUSAT = Date.now()
+
+test(`before testing`, () => {
+    //console.log()
+    expect(fs.statSync("/tmp").isDirectory()).toBe(true);
+    expect(fs.existsSync(`/tmp/UNIXUSAT${UNIXUSAT}`)).toBe(false);
+    // safeMkdir(`/tmp/UNIXUSAT${UNIXUSAT}`)
+    // expect(fs.statSync(`/tmp/UNIXUSAT${UNIXUSAT}`).isDirectory()).toBe(true);
+});
+test(`trying to make "/tmp/UNIXUSAT${UNIXUSAT}" as new dir`, () => {
+    //console.log()
+    //expect(fs.statSync("/tmp").isDirectory()).toBe(true);
+    //expect(fs.existsSync(`/tmp/UNIXUSAT${UNIXUSAT}`)).toBe(false);
+    const results = safeMkdir(`/tmp/UNIXUSAT${UNIXUSAT}`);
+    expect(results[0]).toBe(true);
+    expect(results[1]).toBe(`/tmp/UNIXUSAT${UNIXUSAT}`);
+    expect(fs.existsSync(`/tmp/UNIXUSAT${UNIXUSAT}`)).toBe(true);
+    expect(fs.statSync(`/tmp/UNIXUSAT${UNIXUSAT}`).isDirectory()).toBe(true);
+    //expect(results[2].length).toBe(0);
+});
+test(`trying to make "/tmp/UNIXUSAT${UNIXUSAT}" again`, () => {
+    //console.log()
+    //expect(fs.statSync("/tmp").isDirectory()).toBe(true);
+    //expect(fs.existsSync(`/tmp/UNIXUSAT${UNIXUSAT}`)).toBe(false);
+    const results = safeMkdir("/tmp", `UNIXUSAT${UNIXUSAT}`);
+    expect(results[0]).toBe(false);
+    expect(results[1]).toBe(`/tmp/UNIXUSAT${UNIXUSAT}`);
+    //expect(results[2].length).toBe(1);
+    //expect(fs.existsSync(`/tmp/UNIXUSAT${UNIXUSAT}`)).toBe(true);
+    //expect(fs.statSync(`/tmp/UNIXUSAT${UNIXUSAT}`).isDirectory()).toBe(true);
+});
